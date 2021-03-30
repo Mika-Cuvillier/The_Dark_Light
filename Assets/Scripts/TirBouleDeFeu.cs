2f4729e9ef6277019d7e298b80c657d583c9f6bd
MarcSicotte <Sicotte13@live.fr>
2021-03-30 10:06:17 -0400

    changement de couleur UI

--- Assets/Scripts/TirBouleDeFeu.cs
+++ Assets/Scripts/TirBouleDeFeu.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class TirBouleDeFeu : MonoBehaviour
 {
     public GameObject bouleDeFeu;
+    public Image jaugeFeu;
     public float vitesseBouleDeFeu;
     private bool peutTirer;
 
@@ -26,20 +29,27 @@ public class TirBouleDeFeu : MonoBehaviour
     }
 
     void Tir(){
+        jaugeFeu.fillAmount -= 1f; // Descend la jauge de feu au minimum après avoir tirer Marc-Antoine Sicotte 2021-03-30
         peutTirer = false;
         Invoke("ActiveTir", 5f);
         Invoke("NouvelleBouleDeFeu", 0.4f);
     }
 
+
+
+
     void ActiveTir(){
         peutTirer = true;
+        jaugeFeu.fillAmount += 1f; // Remonte la jauge de feu au maximum après avoir reactiver le tir Marc-Antoine Sicotte 2021-03-30
+
     }
 
     void EnleverAnimationBouleDeFeu(){
         GetComponent<Animator>().SetBool("bouleDeFeu", false);
     }
 
-    void NouvelleBouleDeFeu(){
+    void NouvelleBouleDeFeu()
+    {
         GameObject nouvelleBoule = Instantiate(bouleDeFeu, bouleDeFeu.transform.position, bouleDeFeu.transform.rotation);
         nouvelleBoule.SetActive(true);
         nouvelleBoule.GetComponent<Rigidbody>().velocity = nouvelleBoule.transform.forward * vitesseBouleDeFeu;
