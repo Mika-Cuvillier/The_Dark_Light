92da49477590c39ebadccfd484d432fded0de1ec
MarcSicotte <Sicotte13@live.fr>
2021-04-13 09:26:22 -0400

    HUD finale

--- Assets/Scripts/GestionInterface.cs
+++ Assets/Scripts/GestionInterface.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GestionInterface : MonoBehaviour
 {
     // Script pour mettre le jeu � l'arr�t Marc-Antoine Sicotte 2021-03-24
     public GameObject lePerso;
-    // Start is called before the first frame update
-  
+    public GameObject slime;
+    public GameObject fantome;
+    public GameObject chauvesouris;
+    public GameObject lapin;
 
-     public void ArretJeu() 
+    // Start is called before the first frame update
+
+
+    public void ArretJeu() 
     {
+        slime.GetComponent<NavMeshAgent>().enabled = false;
+        fantome.GetComponent<NavMeshAgent>().enabled = false;
+        chauvesouris.GetComponent<NavMeshAgent>().enabled = false;
+        lapin.GetComponent<NavMeshAgent>().enabled = false;
         lePerso.GetComponent<Animator>().enabled = false;
         DeplacementPersonnage.jeuPause = true;
         RotationCam3emePersonne.jeuPause = true;
@@ -18,6 +28,10 @@ public class GestionInterface : MonoBehaviour
 
     public void RelancerJeu()
     {
+        slime.GetComponent<NavMeshAgent>().enabled = true;
+        fantome.GetComponent<NavMeshAgent>().enabled = true;
+        chauvesouris.GetComponent<NavMeshAgent>().enabled = true;
+        lapin.GetComponent<NavMeshAgent>().enabled = true;
         lePerso.GetComponent<Animator>().enabled = true;
         DeplacementPersonnage.jeuPause = false;
         RotationCam3emePersonne.jeuPause = false;
