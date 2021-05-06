68b8e8d409a92e78da7e436e376616cb552cd883
Mika Cuvillier <mika.cuvillier@gmail.com>
2021-05-06 14:41:58 -0400

    collidergrotte

---
+++ Assets/Scripts/OuverturesDeZones.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OuverturesDeZones : MonoBehaviour
+{
+    public bool pngFinalNaufrage;
+   
+    void Update()
+    {
+        if(pngFinalNaufrage == true){
+            
+        }
+    }
+}
