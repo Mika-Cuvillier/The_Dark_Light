5e561ed78f01b14f93bf82be27f86688703d941f
Mika Cuvillier <mika.cuvillier@gmail.com>
2021-05-06 17:08:18 -0400

    Knockback

--- Assets/Scripts/AttaqueEpee.cs
+++ Assets/Scripts/AttaqueEpee.cs
@@ -8,32 +8,32 @@ public class AttaqueEpee : MonoBehaviour
     /// PAR MIKA CUVILLIER
     /// ******************
 
-    private float forceKnockback =10;
+    private float forceKnockback =500;
 
     private void OnCollisionEnter(Collision infoCollisions)
     {
         // Si l'�p�e entre en collision avec l'ennemi on appelle la fonction attaqueepee dans le scipt de l'ennemi
         if(infoCollisions.gameObject.tag == "Ennemi1"){ 
             infoCollisions.gameObject.GetComponent<Ennemis>().AttaqueEpee();
-            Vector3 direction = infoCollisions.transform.position - transform.position;
+            Vector3 direction = infoCollisions.transform.position - transform.parent.position;
             direction.y = 0;
             infoCollisions.gameObject.GetComponent<Rigidbody>().AddForce(direction.normalized * forceKnockback, ForceMode.Impulse);  
         }
         if(infoCollisions.gameObject.tag == "Ennemi2"){ 
             infoCollisions.gameObject.GetComponent<Ennemis>().AttaqueEpee(); 
-            Vector3 direction = infoCollisions.transform.position - transform.position;
+            Vector3 direction = infoCollisions.transform.position - transform.parent.position;
             direction.y = 0;
             infoCollisions.gameObject.GetComponent<Rigidbody>().AddForce(direction.normalized * forceKnockback, ForceMode.Impulse); 
         }
         if(infoCollisions.gameObject.tag == "Ennemi3"){ 
             infoCollisions.gameObject.GetComponent<Ennemis>().AttaqueEpee(); 
-            Vector3 direction = infoCollisions.transform.position - transform.position;
+            Vector3 direction = infoCollisions.transform.position - transform.parent.position;
             direction.y = 0;
             infoCollisions.gameObject.GetComponent<Rigidbody>().AddForce(direction.normalized * forceKnockback, ForceMode.Impulse); 
         }
         if(infoCollisions.gameObject.tag == "Ennemi4"){ 
             infoCollisions.gameObject.GetComponent<Ennemis>().AttaqueEpee(); 
-            Vector3 direction = infoCollisions.transform.position - transform.position;
+            Vector3 direction = infoCollisions.transform.position - transform.parent.position;
             direction.y = 0;
             infoCollisions.gameObject.GetComponent<Rigidbody>().AddForce(direction.normalized * forceKnockback, ForceMode.Impulse); 
         }
