cd0762b135e68f19dd6a124075db304c8aa19a82
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-05-11 12:53:55 -0400

    Coffre terminé

--- Assets/Scripts/GestionParchemin.cs
+++ Assets/Scripts/GestionParchemin.cs
@@ -10,7 +10,6 @@ public class GestionParchemin : MonoBehaviour
     public GameObject leParchemin; // Référence au parchemin désiré
     public GameObject topCoffre; // référence au dessus du coffre
 
-    // Start is called before the first frame update
     public void GestionParcheminTest()
     {
         // Désactiver le personnage et les ennemis
