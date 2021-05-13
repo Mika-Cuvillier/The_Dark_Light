2a68a10ac19c8300eabd4e96c149f47ce3e45aff
Mika Cuvillier <mika.cuvillier@gmail.com>
2021-05-13 15:28:07 -0400

    son knockback

--- Assets/Scripts/AttaqueEpee.cs
+++ Assets/Scripts/AttaqueEpee.cs
@@ -9,30 +9,40 @@ public class AttaqueEpee : MonoBehaviour
     /// ******************
 
     private float forceKnockback =250;
+    public AudioClip sonKnockback;
+    public Material MatSlime;
 
     private void OnCollisionEnter(Collision infoCollisions)
     {
         // Si l'�p�e entre en collision avec l'ennemi on appelle la fonction attaqueepee dans le scipt de l'ennemi
         if(infoCollisions.gameObject.tag == "Ennemi1"){ 
             infoCollisions.gameObject.GetComponent<Ennemis>().AttaqueEpee();
+            infoCollisions.gameObject.GetComponent<AudioSource>().PlayOneShot(sonKnockback, 2f);
+            infoCollisions.gameObject.GetComponent<Renderer>().material.color = Color.red;
             Vector3 direction = infoCollisions.transform.position - transform.parent.position;
             direction.y = 0;
             infoCollisions.gameObject.GetComponent<Rigidbody>().AddForce(direction.normalized * forceKnockback, ForceMode.Impulse);  
         }
         if(infoCollisions.gameObject.tag == "Ennemi2"){ 
-            infoCollisions.gameObject.GetComponent<Ennemis>().AttaqueEpee(); 
+            infoCollisions.gameObject.GetComponent<Ennemis>().AttaqueEpee();
+            infoCollisions.gameObject.GetComponent<AudioSource>().PlayOneShot(sonKnockback, 2f);
+            infoCollisions.gameObject.GetComponent<Renderer>().material = MatSlime;
             Vector3 direction = infoCollisions.transform.position - transform.parent.position;
             direction.y = 0;
             infoCollisions.gameObject.GetComponent<Rigidbody>().AddForce(direction.normalized * forceKnockback, ForceMode.Impulse); 
         }
         if(infoCollisions.gameObject.tag == "Ennemi3"){ 
             infoCollisions.gameObject.GetComponent<Ennemis>().AttaqueEpee(); 
+            infoCollisions.gameObject.GetComponent<AudioSource>().PlayOneShot(sonKnockback, 2f);
+            infoCollisions.gameObject.GetComponent<Renderer>().material.color = Color.red;
             Vector3 direction = infoCollisions.transform.position - transform.parent.position;
             direction.y = 0;
             infoCollisions.gameObject.GetComponent<Rigidbody>().AddForce(direction.normalized * forceKnockback, ForceMode.Impulse); 
         }
         if(infoCollisions.gameObject.tag == "Ennemi4"){ 
             infoCollisions.gameObject.GetComponent<Ennemis>().AttaqueEpee(); 
+            infoCollisions.gameObject.GetComponent<AudioSource>().PlayOneShot(sonKnockback, 2f);
+            infoCollisions.gameObject.GetComponent<Renderer>().material.color = Color.red;
             Vector3 direction = infoCollisions.transform.position - transform.parent.position;
             direction.y = 0;
             infoCollisions.gameObject.GetComponent<Rigidbody>().AddForce(direction.normalized * forceKnockback, ForceMode.Impulse); 
