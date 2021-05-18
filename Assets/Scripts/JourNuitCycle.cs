a2a8717c88109d9000a76bebc86c7f8c4ed4d7fa
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-05-17 22:36:59 -0400

    Ambiance Village + Training

--- Assets/Scripts/JourNuitCycle.cs
+++ Assets/Scripts/JourNuitCycle.cs
@@ -16,12 +16,9 @@ public class JourNuitCycle : MonoBehaviour
     public Material CielDemiJour;
 
     // LUMIÈRES
-    public GameObject LumierePointJour;
-    public GameObject LumierePointNuit;
-    public GameObject LumierePointTwilight;
-    public GameObject LumiereDirectionalJour;
-    public GameObject LumiereDirectionalNuit;
-    public GameObject LumiereDirectionalTwilight;
+    public GameObject LumieresJour;
+    public GameObject LumieresNuit;
+    public GameObject LumieresTwilight;
 
     // MUSIQUES
     public GameObject MusiqueOn;
@@ -31,61 +28,55 @@ public class JourNuitCycle : MonoBehaviour
     void Start()
     {
         // On commence le jeu le jour
-        Invoke("LumiereJour", 1f);
+        Invoke("AmbianceNuit", 0.1f);
     }
 
-    void LumiereJour ()
+    void AmbianceJour ()
     {
         // On active les différentes lumières relié aux jour et on change le skybox du jeu
         RenderSettings.skybox = CielJour;
-        LumiereDirectionalJour.SetActive(true);
-        LumierePointJour.SetActive(true);
+        LumieresJour.SetActive(true);
 
         //On désactive les lumières de nuits
-        LumiereDirectionalNuit.SetActive(false);
-        LumierePointNuit.SetActive(false);
+        LumieresNuit.SetActive(false);
 
         // Changement de l'AudioClip pour la musique de jour
         MusiqueOn.GetComponent<AudioSource>().clip = jourMusique;
         MusiqueOn.GetComponent<AudioSource>().Play();
 
         // Appel de la fonction pour activer le cycle du crépuscule
-        Invoke("LumiereTwilight", 30f);
+        Invoke("AmbianceTwilight", 30f);
     }
 
     // 
-    void LumiereTwilight()
+    void AmbianceTwilight()
     {
         // On active les différentes lumières du crépuscule et on change le skybox du jeu
         RenderSettings.skybox = CielDemiJour;
-        LumiereDirectionalTwilight.SetActive(true);
-        LumierePointTwilight.SetActive(true);
+        LumieresTwilight.SetActive(true);
 
         // On désactive les lumières de jours
-        LumiereDirectionalJour.SetActive(false);
-        LumierePointJour.SetActive(false);
+        LumieresJour.SetActive(false);
 
         // Appel de la fonction pour activer le cycle de la nuit
-        Invoke("LumiereNuit", 20f);
+        Invoke("AmbianceNuit", 20f);
     }
 
-    void LumiereNuit ()
+    void AmbianceNuit ()
     {
         // On active les différentes lumières de nuit et on change le skybox du jeu
         RenderSettings.skybox = CielNuit;
-        LumiereDirectionalNuit.SetActive(true);
-        LumierePointNuit.SetActive(true);
+        LumieresNuit.SetActive(true);
 
         // On désactive les lumières du crépuscule
-        LumiereDirectionalTwilight.SetActive(false);
-        LumierePointTwilight.SetActive(false);
+        LumieresTwilight.SetActive(false);
 
         // Changement de l'AudioClip pour la musique de nuit
         MusiqueOn.GetComponent<AudioSource>().clip = nuitMusique;
         MusiqueOn.GetComponent<AudioSource>().Play();
 
         // Appel de la fonction pour activer le cycle du jour
-        Invoke("LumiereJour", 50f);
+        Invoke("AmbianceJour", 150f);
     }
 
 }
