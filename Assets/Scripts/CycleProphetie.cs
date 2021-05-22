48a381e3f5d96972c716ed3b421dd0494e923d01
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-05-21 21:43:22 -0400

    Cycle complet for all level

---
+++ Assets/Scripts/CycleProphetie.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CycleProphetie : MonoBehaviour
+{
+    /// ************************
+    /// PAR TAMYLA AIT-CHELLOUCHE
+    /// *************************
+
+    // SKYBOX
+    public Material CielJour;
+    public Material CielNuit;
+    public Material CielDemiJour;
+
+    // LUMIÈRES
+    public GameObject LumieresJour;
+    public GameObject LumieresNuit;
+    public GameObject LumieresTwilight;
+
+    // MUSIQUES
+    public GameObject MusiqueOn;
+    public AudioClip jourMusique;
+    public AudioClip nuitMusique;
+
+    void Start()
+    {
+        // On commence le jeu le jour
+        Invoke("AmbianceJour", 0.1f);
+    }
+
+    void AmbianceJour()
+    {
+        // On active les différentes lumières relié aux jour et on change le skybox du jeu
+        RenderSettings.skybox = CielJour;
+        LumieresJour.SetActive(true);
+
+        //On désactive les lumières de nuits
+        LumieresNuit.SetActive(false);
+
+        // Changement de l'AudioClip pour la musique de jour
+        MusiqueOn.GetComponent<AudioSource>().clip = jourMusique;
+        MusiqueOn.GetComponent<AudioSource>().Play();
+
+        // Appel de la fonction pour activer le cycle du crépuscule
+        Invoke("AmbianceTwilight", 100f);
+    }
+
+    // 
+    void AmbianceTwilight()
+    {
+        // On active les différentes lumières du crépuscule et on change le skybox du jeu
+        RenderSettings.skybox = CielDemiJour;
+        LumieresTwilight.SetActive(true);
+
+        // On désactive les lumières de jours
+        LumieresJour.SetActive(false);
+
+        // Appel de la fonction pour activer le cycle de la nuit
+        Invoke("AmbianceNuit", 200f);
+    }
+
+    void AmbianceNuit()
+    {
+        // On active les différentes lumières de nuit et on change le skybox du jeu
+        RenderSettings.skybox = CielNuit;
+        LumieresNuit.SetActive(true);
+
+        // On désactive les lumières du crépuscule
+        LumieresTwilight.SetActive(false);
+
+        // Changement de l'AudioClip pour la musique de nuit
+        MusiqueOn.GetComponent<AudioSource>().clip = nuitMusique;
+        MusiqueOn.GetComponent<AudioSource>().Play();
+    }
+}
